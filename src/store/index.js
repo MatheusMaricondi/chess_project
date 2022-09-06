9322d42061ae1ad0c97293df20418fa3d6556164
Matheus Maricondi <matheus.maricondi@gmail.com>
2022-09-06 14:33:15 -0300

    master - features

--- src/store/index.js
+++ src/store/index.js
@@ -1,25 +1,25 @@
 import { writable, derived } from 'svelte/store';
 
-// let position_pieces = writable([
-//   ['R','N','B','Q','K','B','N','R'],
-//   ['P','P','P','P','P','P','P','P'],
-//   [' ',' ',' ',' ',' ',' ',' ',' '],
-//   [' ',' ',' ',' ',' ',' ',' ',' '],
-//   [' ',' ',' ',' ',' ',' ',' ',' '],
-//   [' ',' ',' ',' ',' ',' ',' ',' '],
-//   ['p','p','p','p','p','p','p','p'],
-//   ['r','n','b','q','k','b','n','r']
-// ])
 let position_pieces = writable([
-  [' ',' ',' ',' ','K',' ',' ','R'],
-  [' ',' ','P',' ',' ','N',' ',' '],
+  ['R','N','B','Q','K','B','N','R'],
+  ['P','P','P','P','P','P','P','P'],
   [' ',' ',' ',' ',' ',' ',' ',' '],
   [' ',' ',' ',' ',' ',' ',' ',' '],
-  ['p',' ',' ',' ',' ',' ',' ',' '],
   [' ',' ',' ',' ',' ',' ',' ',' '],
-  [' ','b','k',' ',' ',' ',' ',' '],
-  [' ',' ',' ',' ',' ',' ',' ',' ']
+  [' ',' ',' ',' ',' ',' ',' ',' '],
+  ['p','p','p','p','p','p','p','p'],
+  ['r','n','b','q','k','b','n','r']
 ])
+// let position_pieces = writable([
+//   [' ',' ',' ',' ','K',' ',' ','R'],
+//   [' ',' ','P',' ',' ','N',' ',' '],
+//   [' ',' ',' ',' ',' ',' ',' ',' '],
+//   [' ',' ',' ',' ',' ',' ',' ',' '],
+//   ['p',' ',' ',' ',' ',' ',' ',' '],
+//   [' ',' ',' ',' ',' ',' ',' ',' '],
+//   [' ','b','k',' ',' ',' ',' ',' '],
+//   [' ',' ',' ',' ',' ',' ',' ',' ']
+// ])
 
 let promotion_modal_object = {
   promotion_modal: writable(false),
@@ -34,14 +34,14 @@ let modifiers = {
   black_castle_: writable({}),
   en_passant: writable({position: null}), // position by column 0 .. 7
   en_passant_: writable({}),
-  game_settings: writable({white_turn: false, white_player: false}), // white true
+  game_settings: writable({white_turn: true, white_player: true}), // white true
   game_settings_: writable({}),
   engine_settings: writable({deep: 1, nodes: 2}),
   engine_settings_: writable({}),
   xeque_mate: writable(null), // 0: draw, 1: player1 win, 2: enigine win
   xeque_mate_: writable(null),
-  pieces: writable({white: {p: 1, r: 0, n: 0, wb: 0, bb: 0, q: 0}, black: {P: 1, R: 1, N: 1, WB: 0, BB: 0, Q: 0}}),
-  pieces_: writable({white: {p: 1, r: 0, n: 0, wb: 0, bb: 0, q: 0}, black: {P: 1, R: 1, N: 1, WB: 0, BB: 0, Q: 0}}),
+  pieces: writable({white: {p: 8, r: 2, n: 2, wb: 1, bb: 1, q: 1}, black: {P: 7, R: 2, N: 2, WB: 1, BB: 1, Q: 1}}),
+  pieces_: writable({white: {p: 8, r: 2, n: 2, wb: 1, bb: 1, q: 1}, black: {P: 7, R: 1, N: 1, WB: 0, BB: 0, Q: 0}}),
 }
 
 
